d77815db4f57603c745045faffb4adcbb677fcd4
yusufkurnia <42245260+yusufkurnia@users.noreply.github.com>
2025-10-03 02:30:02 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,13 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'cabin-sketch': ['Cabin Sketch', 'cursive'],
+      },
+    },
   },
   plugins: [],
 }
